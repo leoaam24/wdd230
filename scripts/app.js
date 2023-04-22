4ab1ddb4536c33b86650285c4d789c1e84983957
leoaam24 <leo.a.mosquera@gmail.com>
2023-04-22 14:03:42 +0800

    Assignment Portal Page

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+//toLocalceDateString
+const options = {weekday: 'long', day:'numeric', month: 'long', year:'numeric'};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+// Long hand method ... building day and month names from built-in date methods.
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const modified = document.lastModified;
+document.querySelector("#currentyear").textContent = year;
+document.querySelector("#lastmodified").textContent = `Last Updated: ${modified}`;
+
