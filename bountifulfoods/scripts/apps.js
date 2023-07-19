c1c374d4e64da57a3bac9046511f4ab45c7a2c88
leoaam24 <leo.a.mosquera@gmail.com>
2023-07-19 14:40:01 +0800

    Optimization update Bountiful Foods

--- bountifulfoods/scripts/apps.js
+++ bountifulfoods/scripts/apps.js
@@ -1,4 +1,11 @@
-if (window.matchMedia('(min-width: 699px)').matches) {
+const mediaSmall = window.matchMedia("(max-width: 699px)");
+
+mediaSmall.addEventListener('change', screenSizeSmall);
+
+screenSizeSmall(mediaSmall);
+
+function screenSizeSmall(eventSmall) {
+    if (eventSmall.matches) {
     const navbuttonopen = document.querySelector('#navButtonOpen');
     const navbuttonclose = document.querySelector('#navButtonClose');
     const sidenavcontainer = document.querySelector('#mySideNav');
@@ -13,14 +20,13 @@ if (window.matchMedia('(min-width: 699px)').matches) {
     if (weatherInfo) {
         weatherInfo.style.display = "none";
     }
-    
     })
 
     const sidenav = document.querySelector('.sidenav');
     const liElements = document.querySelectorAll('.sidenav li');
 
     for (let i = 0; i < liElements.length; i++) {
-    liElements[i].addEventListener('mouseover', function() {
+        liElements[i].addEventListener('mouseover', function() {
         sidenav.style.overflowX = 'unset';
     });
 
@@ -34,13 +40,8 @@ if (window.matchMedia('(min-width: 699px)').matches) {
     navbuttonopen.style.display = "unset";
     navbuttonclose.style.display = "none";
     sidenavbackground.style.display = "none";
-    if (document.querySelector('.order-form-container')) {
-        weatherInfo.style.display = "none";
-    } else {
-        if (weatherInfo){
-            weatherInfo.style.display = "unset";
-        }
-        
+    if (weatherInfo) {
+        weatherInfo.style.display = "unset";
     }
     })
     
@@ -48,6 +49,86 @@ if (window.matchMedia('(min-width: 699px)').matches) {
     const navbuttonopen = document.querySelector('#navButtonOpen');
     navbuttonopen.style.display = "none";
 }
+
+const orderCount = localStorage.getItem('successfulorders');
+const htmlordercount = document.querySelector('#orderCount');
+
+if (document.querySelector('.hero-container')){
+    if (orderCount === null){
+        htmlordercount.textContent = `Welcome! Discover the pure essence of fruits in every sip!`
+    } else if (parseInt(orderCount) === 1){
+        htmlordercount.textContent = `How's your 1st order? Don't miss out! Resdiscover the deliciousness of our drink!`
+    } else if (parseInt(orderCount) > 1){
+        htmlordercount.textContent = `We're glad to have you back! Orders made: ${orderCount} 👏🏻`;
+    }
+}
+
+if (document.querySelector('.aboutus-container')){
+    const sendfeedbackform = document.querySelector('.sendfeedback-form');
+
+    sendfeedbackform.addEventListener('submit', () => {
+        alert('Message Sent!');
+    })
+}
+
+if (document.querySelector('.hero-container')){
+    const freshbutton = document.querySelector('#freshButton');
+
+    freshbutton.addEventListener('click', () => {
+        window.location.href = 'freshpage.html';
+    });
+}
+
+}
+
+//     const navbuttonopen = document.querySelector('#navButtonOpen');
+//     const navbuttonclose = document.querySelector('#navButtonClose');
+//     const sidenavcontainer = document.querySelector('#mySideNav');
+//     const sidenavbackground = document.querySelector('.sidenavbackground');
+//     const weatherInfo = document.querySelector('.weather-info');
+
+//     navbuttonopen.addEventListener('click', ()=> {
+//     sidenavcontainer.style.width = "40%";
+//     navbuttonopen.style.display = "none";
+//     navbuttonclose.style.display = "unset";
+//     sidenavbackground.style.display = "unset";
+//     if (weatherInfo) {
+//         weatherInfo.style.display = "none";
+//     }
+//     })
+
+//     const sidenav = document.querySelector('.sidenav');
+//     const liElements = document.querySelectorAll('.sidenav li');
+
+//     for (let i = 0; i < liElements.length; i++) {
+//     liElements[i].addEventListener('mouseover', function() {
+//         sidenav.style.overflowX = 'unset';
+//     });
+
+//     liElements[i].addEventListener('mouseout', function() {
+//         sidenav.style.overflowX = '';
+//     });
+//     }
+
+//     navbuttonclose.addEventListener('click', ()=>{
+//     sidenavcontainer.style.width = "0%";
+//     navbuttonopen.style.display = "unset";
+//     navbuttonclose.style.display = "none";
+//     sidenavbackground.style.display = "none";
+//     if (document.querySelector('.order-form-container')) {
+//         weatherInfo.style.display = "none";
+//     } else {
+//         if (weatherInfo){
+//             weatherInfo.style.display = "unset";
+//         }
+        
+//     }
+//     })
+    
+// } else {
+//     const navbuttonopen = document.querySelector('#navButtonOpen');
+//     navbuttonopen.style.display = "none";
+// }
 
 
 // Long hand method ... building day and month names from built-in date methods.
@@ -84,37 +165,35 @@ if (window.matchMedia('(min-width: 699px)').matches) {
     document.querySelector("#currentyear").textContent = year;
     document.querySelector("#lastmodified").textContent = modified;
 
-    if (document.querySelector('.order-form-container')) {
-        document.querySelector('.weather-info').style.display = 'none';
-    }
+   // if (window.matchMedia('(min-width: 699px)').matches) {
 
-    const orderCount = localStorage.getItem('successfulorders');
-    const htmlordercount = document.querySelector('#orderCount');
+    // const orderCount = localStorage.getItem('successfulorders');
+    // const htmlordercount = document.querySelector('#orderCount');
 
-    if (document.querySelector('.hero-container')){
-        if (orderCount === null){
-            htmlordercount.textContent = `Welcome! Discover the pure essence of fruits in every sip!`
-        } else if (parseInt(orderCount) === 1){
-            htmlordercount.textContent = `How's your 1st order? Don't miss out! Resdiscover the deliciousness of our drink!`
-        } else if (parseInt(orderCount) > 1){
-            htmlordercount.textContent = `We're glad to have you back! Orders made: ${orderCount} 👏🏻`;
-        }
-    }
+    // if (document.querySelector('.hero-container')){
+    //     if (orderCount === null){
+    //         htmlordercount.textContent = `Welcome! Discover the pure essence of fruits in every sip!`
+    //     } else if (parseInt(orderCount) === 1){
+    //         htmlordercount.textContent = `How's your 1st order? Don't miss out! Resdiscover the deliciousness of our drink!`
+    //     } else if (parseInt(orderCount) > 1){
+    //         htmlordercount.textContent = `We're glad to have you back! Orders made: ${orderCount} 👏🏻`;
+    //     }
+    // }
 
-    if (document.querySelector('.aboutus-container')){
-        const sendfeedbackform = document.querySelector('.sendfeedback-form');
+    // if (document.querySelector('.aboutus-container')){
+    //     const sendfeedbackform = document.querySelector('.sendfeedback-form');
 
-        sendfeedbackform.addEventListener('submit', () => {
-            alert('Message Sent!');
-        })
-    }
+    //     sendfeedbackform.addEventListener('submit', () => {
+    //         alert('Message Sent!');
+    //     })
+    // }
 
-    if (document.querySelector('.hero-container')){
-        const freshbutton = document.querySelector('#freshButton');
+    // if (document.querySelector('.hero-container')){
+    //     const freshbutton = document.querySelector('#freshButton');
 
-        freshbutton.addEventListener('click', () => {
-            window.location.href = 'freshpage.html';
-        });
-    }
+    //     freshbutton.addEventListener('click', () => {
+    //         window.location.href = 'freshpage.html';
+    //     });
+    // }
 
     
